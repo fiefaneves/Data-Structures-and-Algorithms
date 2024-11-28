2cc4bf61d094133468977d0f2cda73cb31c9c195
fmn <fmn@g1c21.cin.ufpe.br>
2024-11-28 11:37:19 -0300

    aula 6 - array e matriz

---
+++ Object-Oriented-Data-Structures/aulas/aula6/array.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+        // int arr[5];
+        // for (int i = 0; i < 5; i++){
+            // arr[i] = (2*i)+1;
+            // cout << arr[i] << ' ';
+        // }
+        // cout << '\n';
+
+    //acessando array por ponteiro
+
+        // int arr[5] = {1, 3, 5, 7, 9};
+        // int *p = arr;
+        // for (int i = 0; i < 5; i++){
+        //     cout << p[i] << ' ';
+        // }
+        // cout << '\n';
+
+    //utilizando malloc
+
+        // int *arr;
+        // int n;
+        // cout << "digite o tamanho do array: ";
+        // cin >> n;
+        // arr = (int *) malloc(n * sizeof(int));
+        // for (int i = 0; i < n; i++){
+        //     arr[i] = (2*i)+1;
+        //     cout << arr[i] << ' ';
+        // }    
+        // cout << '\n';
+
+        // free(arr);
+        // for (int i = 0; i < n; i++){
+        //     cout << arr[i] << ' ';
+        // }    
+        // cout << '\n';
+
+    //matriz
+        // int arr[5][4];
+        // for (int i = 0; i < 5; i++){
+        //     for (int j = 0; j < 4; j++){
+        //         arr[i][j] = i+j;
+        //         cout << arr[i][j] << ' ';
+        //     }
+        //     cout << '\n';
+        // }
+
+    return 0;
+}
